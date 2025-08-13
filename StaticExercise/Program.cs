f1a079ab7719d91729212814fadb363c7e3d3c6b
Jared Godfrey <godfreyjared@gmail.com>
2025-08-13 15:31:37 -0600

    Finished part 1.

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace StaticExercise
+﻿using System;
+namespace StaticExercise
 {
-    public class Program
+    class program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var celcius = TempConvertor.FahrenheitToCelsius(68); // 20
+            Console.WriteLine($"Celcius = {celcius}.");
+            var fahrenheit = TempConvertor.CelsiusToFahrenheit(20); // 68
+            Console.WriteLine($"Fahrenheit = {fahrenheit}.");
         }
+
+       
+        
+
     }
+
 }
